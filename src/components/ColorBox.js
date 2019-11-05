fa1970cbb2d1594ec8fbfc86e4705a2207c4974d
mWhitman189 <mwhitman189@gmail.com>
2019-11-05 22:47:47 +0900

    feat: Add growing overlay on copy

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -4,12 +4,32 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      is_showing: false,
+    }
+    this.changeCopyState = this.changeCopyState.bind(this)
+  }
+
+  changeCopyState() {
+    this.setState({ is_showing: true }, () => {
+      setTimeout(() => this.setState({ is_showing: false }), 1400)
+    })
+  }
 
   render() {
     const { name, color } = this.props.background
+    const { is_showing } = this.state
+
     return (
-      <CopyToClipboard text={color}>
+      <CopyToClipboard text={color} onCopy={this.changeCopyState}>
         <div className="ColorBox" style={{ backgroundColor: color }}>
+          <div className={`copy-overlay ${is_showing && "show"}`} style={{ backgroundColor: color }} />
+          <div className={`copy-msg ${is_showing && "show"}`}>
+            <h2>Copied!</h2>
+            <p>{color}</p>
+          </div>
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
