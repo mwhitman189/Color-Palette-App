ee537e88221ac7f9cf963ba25680a8d54c33a30f
mWhitman189 <mwhitman189@gmail.com>
2019-11-06 09:28:56 +0900

    feat: Add luminosity scales to a new color palette object

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Palette from './components/Palette'
 import seedColors from './seedColors.js'
 import './App.css'
+import { generatePalette } from './colorHelpers'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[ 5 ]} />
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Palette {...seedColors[ 5 ]} colorScales={generatePalette(seedColors[ 5 ])} />
+      </div>
+    )
+  }
 }
 
 export default App
