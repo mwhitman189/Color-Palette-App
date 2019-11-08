2e18fde4a630e1df98c9556dbd87d1686d9e5640
mWhitman189 <mwhitman189@gmail.com>
2019-11-09 00:05:00 +0900

    feat: Add snackbar to alert color format change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Slider from 'rc-slider'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import 'rc-slider/assets/index.css'
 import './Navbar.css'
 
@@ -10,15 +13,20 @@ class Navbar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      level: this.props.level,
       format: "hex",
+      open: false,
     }
     this.handleFormat = this.handleFormat.bind(this)
+    this.closeSnackbar = this.closeSnackbar.bind(this)
   }
 
   handleFormat(evt) {
-    this.setState({ format: evt.target.value })
     this.props.changeFormat(evt.target.value)
+    this.setState({ format: evt.target.value, open: true })
+  }
+
+  closeSnackbar() {
+    this.setState({ open: false })
   }
 
   render() {
@@ -27,33 +35,55 @@ class Navbar extends Component {
 
     return (
       <header className="Navbar">
-        <div className="logo">
-          {/* eslint-disable-next-line */}
-          <a href="#">
-            <span className="title">Palette Maker</span>
-            <span className="sub-title">3000</span>
-          </a>
-        </div>
-        <div className="lightness-container">
-          <label htmlFor="lumi-slider">Level: {level}</label>
-          <div className="slider">
-            <Slider
-              onChange={changeLevel}
-              value={level}
-              min={100}
-              max={900}
-              step={100}
-              defaultValue={level}
-            />
+        <div className="left">
+          <div className="logo">
+            {/* eslint-disable-next-line */}
+            <a href="#">
+              <span className="title">Palette Maker</span>
+              <span className="sub-title">3000</span>
+            </a>
+          </div>
+          <div className="lightness-container">
+            <label htmlFor="lumi-slider">Level: {level}</label>
+            <div className="slider">
+              <Slider
+                onChange={changeLevel}
+                value={level}
+                min={100}
+                max={900}
+                step={100}
+                defaultValue={level}
+              />
+            </div>
           </div>
         </div>
-        <div className="select-container">
-          <Select value={format} onChange={this.handleFormat}>
-            <MenuItem value="hex">HEX -- #000</MenuItem>
-            <MenuItem value="rgb">RGB -- (0,0,0)</MenuItem>
-            <MenuItem value="rgba">RGBA -- (0,0,0, 0.5)</MenuItem>
-          </Select>
+        <div className="right">
+          <div className="select-container">
+            <Select value={format} onChange={this.handleFormat}>
+              <MenuItem value="hex">HEX -- #000</MenuItem>
+              <MenuItem value="rgb">RGB -- (0,0,0)</MenuItem>
+              <MenuItem value="rgba">RGBA -- (0,0,0, 0.5)</MenuItem>
+            </Select>
+          </div>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format Changed To <span className="bold">{format}</span></span>}
+          ContentProps={{ "aria-describedby": "message-id" }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     )
   }
