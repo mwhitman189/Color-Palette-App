e85cb1a612a0ff52498eb25a7377b76210c79d62
mWhitman189 <mwhitman189@gmail.com>
2019-11-16 23:52:21 +0900

    refactor: Move colorBox styles to JSS for clarity and modularity

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -8,12 +8,27 @@ import chroma from 'chroma-js'
 
 const styles = {
   root: {
-
+    color: props =>
+      chroma(props.background).luminance() > 0.3 ? '#353b48' : '#eeeeee',
+    width: '20%',
+    height: '25%',
+    margin: '0 auto',
+    padding: 0,
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+    marginBottom: '-5px',
+    '&:hover $copyBtn': {
+      opacity: 1,
+    }
   },
   seeMore: {
-    color: props => chroma(props.background).luminance() > 0.4 ? '#353b48' : '#eeeeee',
+    color: props =>
+      chroma(props.background).luminance() > 0.4 ? '#353b48' : '#eeeeee',
     position: 'absolute',
-    backgroundColor: 'rgba(255,255,255, 0.3)',
+    backgroundColor: props =>
+      chroma(props.background).luminance() > 0.5 ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255,255,255, 0.3)',
     border: 'none',
     borderRadius: '8px 0',
     right: 0,
@@ -23,36 +38,14 @@ const styles = {
     fontSize: '0.8rem',
     lineHeight: '30px',
     textAlign: 'center',
-  },
-  copyBtn: {
-    color: props => chroma(props.background).luminance() > 0.3 ? '#353b48' : '#eeeeee',
-    opacity: '0',
-    display: 'inline-block',
-    borderRadius: '2px',
-    width: '100px',
-    height: '30px',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginLeft: '-50px',
-    marginTop: '-15px',
-    textAlign: 'center',
-    outline: 'none',
-    backgroundColor: 'rgba(255,255,255, 0.3)',
-    border: 'none',
-    fontSize: '1rem',
-    lineHeight: '30px',
-    cursor: 'pointer',
-    transition: '0.4s opacity',
     '&:hover': {
-      opacity: '1',
+      backgroundColor: props =>
+        chroma(props.background).luminance()
+          > 0.4 ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)',
     }
   },
-  colorName: {
-    color: props => chroma(props.background).luminance() > 0.3 ? '#353b48' : '#eeeeee',
-  },
-  backBtn: {
-    color: props => chroma(props.background).luminance() > 0.3 ? '#353b48' : '#eeeeee',
+  copyBtn: {
+    opacity: 0,
     display: 'inline-block',
     borderRadius: '2px',
     width: '100px',
@@ -71,6 +64,84 @@ const styles = {
     cursor: 'pointer',
     transition: '0.4s opacity',
   },
+  backBtn: {
+    display: 'inline-block',
+    borderRadius: '2px',
+    width: '100px',
+    height: '30px',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginLeft: '-50px',
+    marginTop: '-15px',
+    textAlign: 'center',
+    outline: 'none',
+    backgroundColor: 'rgba(255,255,255, 0.3)',
+    border: 'none',
+    fontSize: '1rem',
+    lineHeight: '30px',
+    cursor: 'pointer',
+    transition: '0.4s opacity',
+    opacity: 0,
+  },
+  copyOverlay: {
+    opacity: 0,
+    zIndex: 0,
+    width: '100%',
+    height: '100%',
+    transform: 'scale(0.1)',
+    transition: 'transform 1s ease-in-out',
+    '&.show': {
+      opacity: '1',
+      transform: 'scale(50)',
+      zIndex: 10,
+      position: 'absolute',
+      transition: 'transform 1s ease-in-out',
+    }
+  },
+  copyMsg: {
+    color: props =>
+      chroma(props.background).luminance() > 0.3 ? '#353b48' : '#eeeeee',
+    position: 'fixed',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    flexFlow: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '3rem',
+    transform: 'scale(0.1)',
+    opacity: 0,
+    textShadow: '1px 2px 3px rgba(0, 0, 0, 0.6)',
+    transition: 'all 0.4s ease-in-out',
+    '&.show': {
+      opacity: 1,
+      transform: 'scale(1)',
+      zIndex: 25,
+      transition: 'all 0.4s 0.3s ease-in-out',
+    },
+    '& h2': {
+      background: 'rgba(255,255,255, 0.3)',
+      textAlign: 'center',
+      margin: 0,
+      width: '100%',
+    },
+    '& p': {
+      color: 'rgba(255,255,255, 0.7)',
+      fontSize: '1.5rem',
+      margin: 0,
+    }
+  },
+  boxContent: {
+    position: 'absolute',
+    padding: '10px',
+    left: 0,
+    bottom: 0,
+    fontSize: '0.7rem',
+    letterSpacing: '1px',
+  }
 }
 
 class ColorBox extends Component {
@@ -98,29 +169,21 @@ class ColorBox extends Component {
       showLink,
     } = this.props
     const { is_showing } = this.state
-    const luminance = chroma(background).luminance()
-
-    let textColor
-    if (luminance > 0.3) {
-      textColor = classes.darkText
-    } else {
-      textColor = classes.lightText
-    }
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div className="ColorBox" style={{ backgroundColor: background }}>
+        <div className={`${classes.root}`} style={{ backgroundColor: background }}>
           <div
-            className={`copy-overlay ${is_showing && "show"}`}
+            className={`${classes.copyOverlay} ${is_showing && 'show'}`}
             style={{ backgroundColor: background }}
           />
-          <div className={`copy-msg ${is_showing && "show"}`}>
-            <h2 className={textColor}>Copied!</h2>
+          <div className={`${classes.copyMsg} ${is_showing && 'show'}`}>
+            <h2>Copied!</h2>
             <p>{background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
-              <span className={classes.colorName}>{name}</span>
+          <div>
+            <div className={classes.boxContent}>
+              <span>{name}</span>
             </div>
             <button className={`${classes.copyBtn}`}>Copy</button>
             {showLink && (
